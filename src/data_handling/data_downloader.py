f015b709ea992a642401f4d5c6901faf863137fd
Viktor Marsi <marsi0viktor@gmail.com>
2025-02-14 10:43:39 +0100

    automatic downloading

--- src/data_handling/data_downloader.py
+++ src/data_handling/data_downloader.py
@@ -23,10 +23,27 @@ class DataDownloader:
         if self.data_folder_path is None:
             self.data_folder_path = os.path.join(PROJECT_PATH, 'data')
 
-        self.download_data()
+        if not self.do_all_files_exist():
+            self.download_data()
 
     def download_data(self) -> None:
         """
         Downloads all data from Google Drive.
         """
         gdown.download_folder(url=self.folder_link, output=self.data_folder_path)
+
+    @staticmethod
+    def do_all_files_exist() -> bool:
+        """
+        This function checks whether all the files we wish to download already exist
+
+        :return bool: True if all of them exist, False if at least one is missing
+        """
+
+        files = ["cases_and_deaths_data.csv", "meta.csv"]
+
+        for file in files:
+            if not os.path.exists(os.path.join(PROJECT_PATH, 'data', file)):
+                return False
+
+        return True
